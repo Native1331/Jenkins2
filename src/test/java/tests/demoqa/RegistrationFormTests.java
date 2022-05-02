79c41a55a1adec6eee03c5884dbb848fbcee3990
Natalya.Kadysheva <Native1331>
2022-05-02 13:34:57 +0300

    Add Home work

--- src/test/java/tests/demoqa/RegistrationFormTests.java
+++ src/test/java/tests/demoqa/RegistrationFormTests.java
@@ -1,5 +1,6 @@
 package tests.demoqa;
 
+import com.codeborne.selenide.Condition;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
@@ -11,39 +12,20 @@ import static io.qameta.allure.Allure.step;
 
 @Tag("demoqa")
 public class RegistrationFormTests extends tests.demoqa.TestBase {
-
-    @Test
-    @DisplayName("Successful fill registration test")
-    void fillFormTest() {
-        step("Open registration form", () -> {
-            open("/automation-practice-form");
-            $(".practice-form-wrapper").shouldHave(text("Student Registration Form"));
-            executeJavaScript("$('footer').remove()");
-            executeJavaScript("$('#fixedban').remove()");
-                   });
-        step("Fill registration form", () -> {
-            $("#firstName").setValue("Alex");
-            $("#lastName").setValue("Egorov");
-            $("#userEmail").setValue("alex@egorov.com");
-            $("#genterWrapper").$(byText("Other")).click();
-            $("#userNumber").setValue("1231231230");
-            $("#dateOfBirthInput").click();
-            $(".react-datepicker__month-select").selectOption("July");
-            $(".react-datepicker__year-select").selectOption("2008");
-            $(".react-datepicker__day--030:not(.react-datepicker__day--outside-month)").click();
-            $("#subjectsInput").setValue("Math").pressEnter();
-            $("#hobbiesWrapper").$(byText("Sports")).click();
-            $("#uploadPicture").uploadFromClasspath("img/1.img");
-            $("#currentAddress").setValue("Some street 1");
-            $("#state").click();
-            $("#stateCity-wrapper").$(byText("NCR")).click();
-            $("#city").click();
-            $("#stateCity-wrapper").$(byText("Delhi")).click();
-            $("#submit").click();
-        });
-        step("Verify form data", () -> {
-            $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
-            $(".table-responsive").shouldHave(text("Alex Egorov"), text("alex@egorov.com"), text("Other"));
-        });
+        @Test
+        @DisplayName("Successful  test")
+        void shouldFindSelenide() {
+            //открыть страницу github.com
+            open("https://github.com");
+            //ввести в поле поиска selenide и нажать Enter
+            $("[data-test-selector=nav-search-input]").setValue("selenide").pressEnter();
+            sleep(5000);
+            //нажать на линк от первого результата поиска
+            $$("ul.repo-list li").first().$("a").click();
+            //проверка:
+            $("h2").shouldHave(Condition.text("selenide / selenide"));
+        }
     }
-}
+
+
+
